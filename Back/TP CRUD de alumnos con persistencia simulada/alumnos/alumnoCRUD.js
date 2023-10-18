bd4728e70fc264c35bda26102311db0646ca0d76
Langello <gastonchavo@gmail.com>
2023-10-18 08:22:22 -0300

    [Presentacion del CRUD] Estan hehas las funciones en alumnosCRUD.js pero no funcionan, tenien el problema de permiso.

--- Back/TP CRUD de alumnos con persistencia simulada/alumnos/alumnoCRUD.js
+++ Back/TP CRUD de alumnos con persistencia simulada/alumnos/alumnoCRUD.js
@@ -1,11 +1,23 @@
 import {Alumno} from '../alumnos/alumnoMODEL.js';
 
-
+// Consultar Alumnos
 async function  consultarAlumnos(){
   const alumnos = await Alumno.findAll();
   console.log(alumnos);
 }
 
-consultarAlumnos();
+//consultarAlumnos();
 
 // Salta error de permisos
+
+// Crear Alumno
+async function crearAlumno(){
+  const alumno = await Alumno.create({
+    Nombre: 'Facundo',
+    Apellido: 'Franco',
+    Email: 'fperez@codoacodo.com'
+  });
+  console.log(alumno);
+}
+
+crearAlumno();
